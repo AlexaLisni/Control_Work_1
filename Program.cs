220158f4e8f82aafb18fb400e00528d076eeecc5
Alexandr <sashylay0@gmail.com>
2022-09-19 11:40:42 +0300

    finish task

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+    }
+    Console.Write(array[array.Length - 1]);
+    Console.Write("]");
+}
+
+
+string[] array = { "London", "is", "the", "capital", "of", "Great", "Britan" };
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3) count++;
+}
+
+if (count == 0) count++;
+
+string[] newArray = new string[count];
+
+int j = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        lastArray[j] = array[i];
+        j++;
+    }
+}
+
+PrintArray(newArray);
